d0270011df54d4448409602c47b111aa443df897
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-01-10 08:38:29 -0300

    Finalizando refatoração.

--- core/core/main.py
+++ core/core/main.py
@@ -6,68 +6,69 @@ from decouple import (
 
 class EmailAutomation():
     """
-    Enviar emails.
+    send emails.
     """
+
     def __init__(self, my_email, my_password):
         self.MY_EMAIL = my_email
-        self.MY_senha = my_password
+        self.MY_PASSWORD = my_password
 
-    def enviar_email(
+    def send_email(
         self,
         addressee,
         title=None,
         email_content=None,
         attachments=None
     ):
-        email = self.MY_EMAIL
-        senha = self.MY_senha
 
-        usuario = yagmail.SMTP(user=email, password=senha)
+        user = yagmail.SMTP(
+            user=self.MY_EMAIL,
+            password=self.MY_PASSWORD
+        )
 
-        usuario.send(
+        user.send(
             to=addressee,
             subject=title,
             contents=email_content,
             attachments=attachments,
         )
 
-        print("Email enviado!!!")
+        print("Email sent!!!")
 
 
 if __name__ == "__main__":
-    config = AutoConfig(r'../.env')
+    config = AutoConfig(r'../EmailAutomation_Python/core/.env')
 
     my_email = config('MY_EMAIL')
     my_password = config('MY_PASSWORD')
     my_other_email = config('MY_OTHER_EMAIL')
-    email_title = "A Referência da Linguagem Python."
+    email_title = "What is Python? Executive Summary."
 
     email_content = """
-    <p>Este manual de referência descreve a
-    sintaxe e a “semântica central” da linguagem.
-    É conciso, mas tenta ser exato e completo.
-    A semântica dos tipos de objetos embutidos
-    não essenciais e das funções e módulos embutidos
-    é descrita em A Biblioteca Padrão do Python. Para
-    uma introdução informal à linguagem, consulte O
-    tutorial de Python. Para programadores em C ou C++,
-    existem dois manuais adicionais: Estendendo e
-    Incorporando o Interpretador Python descreve a imagem
-    de alto nível de como escrever um módulo de extensão
-    Python, e o Manual de referência da API Python/C descreve
-    as interfaces disponíveis para programadores C/C++ em detalhes.</p>
+    <p>What is Python? Executive Summary
+    Python is an interpreted, object-oriented,
+    high-level programming language with dynamic semantics.
+    Its high-level built in data structures, combined with dynamic
+    typing and dynamic binding, make it very attractive for Rapid
+    Application Development, as well as for use as a scripting or glue
+    language to connect existing components together. Python's simple,
+    easy to learn syntax emphasizes readability and therefore reduces
+    the cost of program maintenance. Python supports modules and packages,
+    which encourages program modularity and code reuse. The Python interprete
+    and the extensive standard library are available in source or binary form
+    without charge for all major platforms, and can be freely distributed.</p>
 
     <p>ass:. samuelbarbosa_dev</p>
     """
 
-    attach_file = "seu_anexo.pdf"
+    attach_file = None
 
     email = EmailAutomation(
         my_email=my_email,
         my_password=my_password,
     )
 
-    email.enviar_email(
+    email.send_email(
         addressee=my_other_email,
         title=email_title,
         email_content=email_content,
